78842e9220d48078dbd10665eb60d05288a640fc
Aman Siwach <siwachaman51@gmail.com>
2025-10-02 21:34:10 +0530

    minor correction

--- client/src/api/ApiClient.jsx
+++ client/src/api/ApiClient.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = Process.env.API_URL||'http://localhost:5000/api/v1';
+const API_URL = process.env.API_URL||'http://localhost:5000/api/v1';
 
 // Create a centralized axios instance
 const apiClient = axios.create({
