17598f05f354124d941a2945b75b278999d8f2bc
Aman Siwach <siwachaman51@gmail.com>
2025-10-02 22:37:23 +0530

    api url

--- client/src/api/ApiClient.jsx
+++ client/src/api/ApiClient.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.API_URL||'http://localhost:5000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL||'http://localhost:5000/api/v1';
 
 // Create a centralized axios instance
 const apiClient = axios.create({
