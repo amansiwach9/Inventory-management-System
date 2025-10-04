1ad88b21eabb206ad13baaaf2a251b7f757c1392
Aman Siwach <siwachaman51@gmail.com>
2025-10-04 09:55:28 +0530

    rate limit

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import authController from '../controllers/authController.js';
+import { authLimiter } from '../middleware/rateLimiter.js';
 const authRouter = express.Router();
+
+authRouter.use(authLimiter);
 
 authRouter.post('/register', authController.registerUser);
 authRouter.post('/verify-otp', authController.verifyUserOtp);
--- server/utils/email.js
+++ server/utils/email.js
@@ -1,32 +1,31 @@
-import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendOtpEmail = async (to, otp) => {
-    const fromAddress = `"Inventory Management" <${process.env.SENDER_EMAIL}>`;
-    const mailOptions = {
-        from: fromAddress,
-        to: to,
-        subject: 'Your Verification Code',
-        text: `Your OTP for Inventory Management registration is: ${otp}. It will expire in 10 minutes.`,
-        html: `<b>Your OTP for Inventory Management registration is: ${otp}</b><p>It will expire in 10 minutes.</p>`,
-    };
+  
+  const fromEmail = process.env.SENDER_EMAIL;
 
-    try {
-        await transporter.sendMail(mailOptions);
-        console.log('OTP email sent successfully');
-    } catch (error) {
-        console.error('Error sending OTP email:', error);
-        throw new Error('Could not send verification email.');
+  const msg = {
+    to: to,
+    from: fromEmail,
+    subject: 'Your Inventory App Verification Code',
+    text: `Your OTP for registration is: ${otp}. It will expire in 10 minutes.`,
+    html: `<b>Your OTP for registration is: ${otp}</b><p>It will expire in 10 minutes.</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log('OTP email sent successfully via SendGrid API');
+  } catch (error) {
+    console.error('Error sending OTP email via SendGrid API:', error);
+
+    if (error.response) {
+      console.error(error.response.body);
     }
+    throw new Error('Could not send verification email.');
+  }
 };
+
